4a6031ea8648befc4d2e80d755b57817f423c30f
James Whayman <private@jameswhayman.com>
2018-04-25 01:05:46 +0100

    updates to beta, contact, font sizes and mobile styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -43,6 +43,14 @@ let App = {
             slidesToShow: 3,
             prevArrow: '<button type="button" class="slick-prev"></button>',
             nextArrow: '<button type="button" class="slick-next"></button>',
+            responsive: [
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
         });
     },
 
